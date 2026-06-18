af07f48562f47da8369213f311406d87837569fc
seimei-suna <seimei-suna@users.noreply.github.com>
2026-06-18 23:02:07 +0200

    Deploiement Seimei

---
+++ config.js
@@ -0,0 +1,4 @@
+var SUPABASE_URL = "https://ltqltvfjpczdstkfrwpo.supabase.co";
+var SUPABASE_KEY = "sb_publishable_9O3wYyIAy3RZKQw9uq_Tqg_4YtjMGC2";
+var MORGUE_PASSWORD = "morgue2024";
+var SUPERVISEUR_CODE = "seimei_superviseur";
